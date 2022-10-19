f62b0082cd6623d058a1fcbdb486195d2e0fc97a
Ieuan Byers <ieuan@ieuans-air.home>
2022-10-19 20:41:33 +0100

    Added json-server backend

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,58 +1,58 @@
-import { useState, useContext, useEffect } from 'react';
-import Card from './shared/Card';
-import Button from './shared/Button';
-import RatingSelect from './RatingSelect';
-import FeedbackContext from '../context/FeedbackContext';
+import { useState, useContext, useEffect } from 'react'
+import Card from './shared/Card'
+import Button from './shared/Button'
+import RatingSelect from './RatingSelect'
+import FeedbackContext from '../context/FeedbackContext'
 
 function FeedbackForm() {
-  const [text, setText] = useState('');
-  const [rating, setRating] = useState(10);
-  const [btnDisabled, setBtnDisabled] = useState(true);
-  const [message, setMessage] = useState('');
+  const [text, setText] = useState('')
+  const [rating, setRating] = useState(10)
+  const [btnDisabled, setBtnDisabled] = useState(true)
+  const [message, setMessage] = useState('')
 
   const { addFeedback, feedbackEdit, updateFeedback } =
-    useContext(FeedbackContext);
+    useContext(FeedbackContext)
 
   useEffect(() => {
     if (feedbackEdit.edit === true) {
-      setBtnDisabled(false);
-      setText(feedbackEdit.item.text);
-      setRating(feedbackEdit.item.rating);
+      setBtnDisabled(false)
+      setText(feedbackEdit.item.text)
+      setRating(feedbackEdit.item.rating)
     }
-  }, [feedbackEdit]);
+  }, [feedbackEdit])
 
   const handleSubmit = (e) => {
-    e.preventDefault();
+    e.preventDefault()
     if (text.trim().length > 10) {
       const newFeedback = {
         text: text,
         rating: rating,
-      };
-
-      if (feedbackEdit.edit === true) {
-        updateFeedback(feedbackEdit.item.id, newFeedback);
-      } else {
-        addFeedback(newFeedback);
       }
 
-      setText('');
+      if (feedbackEdit.edit === true) {
+        updateFeedback(feedbackEdit.item.id, newFeedback)
+      } else {
+        addFeedback(newFeedback)
+      }
+
+      setText('')
     }
-  };
+  }
 
   const handleTextChange = (e) => {
     if (text === '') {
-      setBtnDisabled(true);
-      setMessage(null);
+      setBtnDisabled(true)
+      setMessage(null)
     } else if (text !== '' && text.trim().length <= 10) {
-      setBtnDisabled(true);
-      setMessage('Text must be at least 10 characters');
+      setBtnDisabled(true)
+      setMessage('Text must be at least 10 characters')
     } else {
-      setMessage(null);
-      setBtnDisabled(false);
+      setMessage(null)
+      setBtnDisabled(false)
     }
 
-    setText(e.target.value);
-  };
+    setText(e.target.value)
+  }
 
   return (
     <Card>
@@ -73,7 +73,7 @@ function FeedbackForm() {
         {message && <div className='message'>{message}</div>}
       </form>
     </Card>
-  );
+  )
 }
 
-export default FeedbackForm;
+export default FeedbackForm
